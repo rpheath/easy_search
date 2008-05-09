230ffc72ff1a8bc7d257a4a57de834a03295f367
rpheath <rpheath@gmail.com>
2008-05-09 14:59:06 -0400

    rearranged setup.rb to have getters at the top

--- lib/easy_search/setup.rb
+++ lib/easy_search/setup.rb
@@ -2,6 +2,24 @@ module RPH
   module EasySearch
     class Setup
       class << self
+        # returns a hash with the keys as the models to be searched
+        # and the values as arrays of columns for the respective model
+        #
+        # Example:
+        #   $> Setup.settings
+        #   $> => {"users"=>[:first_name, :last_name, :email], "projects"=>[:title, :description]}
+        def table_settings
+          @@table_settings ||= HashWithIndifferentAccess.new
+        end
+        
+        # returns an array of keywords that serve as no benefit in a search
+        #
+        # Example:
+        #   $> Setup.dull_keywords
+        #   $> => ['a', 'and', 'but', 'the', ...]
+        def dull_keywords
+          (@@dull_keywords ||= DEFAULT_DULL_KEYWORDS).flatten.uniq
+        end
         
         # accepts a block that specifies the columns
         # to search for each model
@@ -16,6 +34,7 @@ module RPH
           self.class_eval(&block)
         end
         
+        # REQUIRED
         # accepts a block that specifies the columns
         # to search for each model
         #
@@ -30,6 +49,7 @@ module RPH
           self.table_settings          
         end
         
+        # OPTIONAL 
         # allows customization of the dull_keywords setting
         # (can be overwritten or appended)
         #
@@ -55,32 +75,13 @@ module RPH
           self.dull_keywords
         end
         
-        # returns a hash with the key as the models to be searched
-        # and the value as an array of columns for that model
-        #
-        # Example:
-        #   $> Setup.settings
-        #   $> => {"users"=>[:first_name, :last_name, :email], "projects"=>[:title, :description]}
-        def table_settings
-          @@table_settings ||= HashWithIndifferentAccess.new
-        end
-        
-        # returns an array of keywords that serve as no benefit in a search
-        #
-        # Example:
-        #   $> Setup.dull_keywords
-        #   $> => ['a', 'and', 'but', 'the', ...]
-        def dull_keywords
-          (@@dull_keywords ||= DEFAULT_DULL_KEYWORDS).flatten.uniq
-        end
-        
-        # this is the magic that makes `Setup.config' work like it does.
+        # this is the magic that makes `setup_tables' work like it does.
         # once the block is eval'd those missing methods (i.e. "users" and "projects")
-        # will be passed here and the settings hash will be updated with the
-        # key set to the table, and the value set to the columns. this allows the
+        # will be caught here and the table_settings hash will be updated with the
+        # key set to the table, and the value set to the columns. this is what allows the
         # EasySearch plugin to work generically for any rails application.
-        def method_missing(table, *fields)
-          table_settings[table] = fields
+        def method_missing(table, *columns)
+          table_settings[table] = columns
         end
       end
     end
